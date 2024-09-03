79c21ac070d8cfe50d2400f6fe4c5bc246a5eed0
Rafi Syihab <rafisyihab3@gmail.com>
2024-09-03 14:55:10 +0700

    update sesuai dari awal;an bulan

--- swipe_to_get.py
+++ swipe_to_get.py
@@ -2,8 +2,24 @@ import pyautogui as pg
 import keyboard
 import time
 import sys
+import re  # Import regex module
 import utils.helper as hp
 import get_weekly as gw
+
+awalan = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec"
+]
 
 def get_region():
     print('Taruh kursor ke sudut kiri atas area, jika sudah tekan "s"')
@@ -36,7 +52,7 @@ print(f'Posisi akhir: {pos2}')
 # Mendapatkan region price
 region_price = get_region()
 
-# Mendapatkan region tanggal
+# Mendapatkan region tanggal (statis)
 region_date = get_region()
 
 # Memulai pada posisi awal
@@ -50,13 +66,20 @@ distance = pos2[0] - pos[0]
 # Variabel untuk menyimpan deteksi tanggal sebelumnya
 previous_date_detected = ""
 
+# Fungsi untuk memfilter tanggal dari teks berdasarkan awalan
+def filter_date(text, awalan):
+    words = text.split()
+    for i in range(len(words) - 2):  # Pastikan ada cukup kata untuk membentuk tanggal
+        if words[i] in awalan:
+            date_candidate = " ".join(words[i:i+3])
+            if re.match(r'^[A-Za-z]{3} \d{2} \d{4}$', date_candidate):
+                return date_candidate
+    return None
+
 # Loop untuk menggerakkan kursor dari posisi awal ke posisi akhir
 for x_offset in range(0, distance, 1):  # Mengubah posisi x setiap 1 piksel
-    # Menggerakkan kursor dan memperbarui region_date
+    # Menggerakkan kursor
     pg.dragTo(pos[0] + x_offset, pos[1], duration=0.2)
-
-    # Memperbarui posisi region_date secara relatif terhadap pergerakan kursor
-    region_date = (region_date[0] + 1, region_date[1], region_date[2], region_date[3])
 
     # Memeriksa jika tombol 'q' ditekan untuk keluar
     if keyboard.is_pressed('q'):
@@ -65,19 +88,24 @@ for x_offset in range(0, distance, 1):  # Mengubah posisi x setiap 1 piksel
 
     # Mengambil teks dari region harga
     detected_text = hp.get_text_from_region(region_price, config="--psm 7 --oem 1 -l eng", save_debug=True)
-    
+
     # Mengambil teks dari region tanggal
     detected_date = hp.get_text_from_region(region_date, config="--psm 7 --oem 1 -l eng", save_debug=True)
     
-    # Membatasi output ke tiga kata pertama saja
-    detected_date_words = detected_date.split()[:3]
-    detected_date_trimmed = " ".join(detected_date_words)
+    # Filter tanggal yang valid dari teks yang terdeteksi
+    valid_date = filter_date(detected_date, awalan)
 
-    # Hanya mencetak jika deteksi tanggal berbeda dari iterasi sebelumnya
-    if detected_date_trimmed != previous_date_detected:
-        print(f'Deteksi tanggal baru: {detected_date_trimmed}')
-        print(f'Deteksi harga: {detected_text}')
-        previous_date_detected = detected_date_trimmed
+    # Hanya mencetak jika deteksi tanggal berbeda dari iterasi sebelumnya dan valid
+    if valid_date and valid_date != previous_date_detected:
+        # Menggunakan regex untuk mengambil angka dari kata ketiga dalam teks harga
+        price_match = re.search(r'\b\d+(\.\d+)?\b', detected_text.split()[2])
+        price_value = price_match.group(0).replace('.', '') if price_match else 'N/A'
+
+        print(f'Deteksi tanggal baru: {valid_date}')
+        print(f'Deteksi harga: {price_value}')
+
+        # Memperbarui deteksi tanggal sebelumnya
+        previous_date_detected = valid_date
 
     time.sleep(0.3)  # Jeda untuk menjaga kursor tetap aktif dan memicu hover
 
